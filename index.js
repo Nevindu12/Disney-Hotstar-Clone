ef9cb271414c91b9280ba840855626b3255efa26
Nevindu12 <nilangithnevindu@gmail.com>
2023-07-19 01:44:02 +0530

    add image cards & animate

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//carousel
 let movies = [
     {
         name: "Falcon and the Winter Soldier",
@@ -77,3 +78,34 @@ for(let i=0; i<3; i++){
 setInterval(() => {
     createSlide();
 },3000);
+
+//image card
+const videoCards = [...document.querySelectorAll(".video-card")];
+
+videoCards.forEach((item) => {
+    item.addEventListener("mouseover", () => {
+        let video = item.children[1];
+        video.play();
+    });
+    item.addEventListener("mouseleave", () => {
+        let video = item.children[1];
+        video.pause();
+    });
+});
+
+//card slider
+let cardContainers = [...document.querySelectorAll('.card-container')];
+let preBtns = [...document.querySelectorAll('.pre-btn')];
+let nxtBtns = [...document.querySelectorAll('.nxt-btn')];
+
+cardContainers.forEach((item,i) =>{
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth= containerDimensions.width;
+
+    nxtBtns[i].addEventListener('click',()=>{
+        item.scrollLeft += containerWidth - 200;
+    });
+    preBtns[i].addEventListener('click',()=>{
+        item.scrollLeft -= containerWidth + 200;
+    });
+});
